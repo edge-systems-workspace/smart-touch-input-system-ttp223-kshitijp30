4e71e996302afeb77ba6ecabec3f1d04fa6eac85
Kshitij Singh <kshitij.verma_cs.aiml24@gla.ac.in>
2026-02-19 08:41:51 +0530

    add touch detection and LED control in loop function

--- src/main.cpp
+++ src/main.cpp
@@ -12,21 +12,21 @@
 
 int touchPin = 5;
 int ledPin = 13;
-void setup() {
-    pinMode(touchPin,INPUT);
-    pinMode(ledPin,OUTPUT);
-    Serial.begin(9600);
-}
-
 
 void setup() {
     pinMode(touchPin,INPUT);
     pinMode(ledPin,OUTPUT);
     Serial.begin(9600);
 }
-
 
 void loop() {
-
-
+    int state = digitalRead(touchPin);
+    if(state==1){
+        digitalWrite(13,1);
+        Serial.println("ToUcH");
+    }
+    else{
+        digitalWrite(13,0);
+        Serial.println("UnToUcHeD");
+    }
 }
