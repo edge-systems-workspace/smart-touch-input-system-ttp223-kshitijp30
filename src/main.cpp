122b3dd047fb47617d3e6332fbf22ee42706bdf0
Kshitij Singh <kshitij.verma_cs.aiml24@gla.ac.in>
2026-02-19 08:39:46 +0530

    add setup function for touch and LED pin initialization

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,13 @@
  * and displays structured output via Serial Monitor.
  */
 
+int touchPin = 5;
+int ledPin = 13;
+void setup() {
+    pinMode(touchPin,INPUT);
+    pinMode(ledPin,OUTPUT);
+    Serial.begin(9600);
+}
 
 
 void setup() {
